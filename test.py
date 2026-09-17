3ec2c6994afb95cfeadaea1bc6f487a849611a77
PatrikR48 <rehorek_patrik@oauh.cz>
2026-09-17 12:56:32 +0200

    v1.2

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ import os
 with open(os.sep.join(["slozka", "testhod2.txt"]), encoding="utf-8") as soubor:
     obsah = soubor.read()
     print(obsah)
-print("Soubor byl úspěšně načten.")
+print("Data byla úspěšně načtena")
